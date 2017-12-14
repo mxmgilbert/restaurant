218453f852c3f35182ed194834b2e658e410bc6e
Maxime Gilbert <mxm.gilbert@gmail.com>
2017-12-14 17:34:33 +0100

    Querying the DB for stores an display them using a mixin template

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -5,13 +5,22 @@ exports.homePage = (req, res) => {
   res.render('index', { title: 'Welcome' });
 };
 
-// we render the same template to add or edit an existing store
+// We render the same template to add or edit an existing store
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
+// Create new store in MongoDB using Async / Await
 exports.createStore = async (req, res) => {
+  // We wait to the store to come back, this way we can use the slug
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review ?`);
   res.redirect(`/store/${store.slug}`);
+};
+
+// Get the store from DB and pass it to the template
+exports.getStores = async (req, res) => {
+  // Query the database for a list of all stores
+  const stores = await (Store.find());
+  res.render('stores', {title: 'Stores', stores});
 };
